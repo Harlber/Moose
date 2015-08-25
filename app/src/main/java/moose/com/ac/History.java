5513e9d2becde98ae878dd47a560358a9b948b7f
Harlber <1044698769@qq.com>
2015-08-25 21:40:04 +0800

    start history steps

---
+++ app/src/main/java/moose/com/ac/History.java
@@ -0,0 +1,26 @@
+package moose.com.ac;
+
+import moose.com.ac.ui.ArticleListActivity;
+
+/**
+ * Created by Farble on 2015/8/25 21.
+ * history steps
+ */
+public class History extends ArticleListActivity {
+    private static final String TAG = "History";
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void loadMore() {
+
+    }
+
+    @Override
+    protected void initToolBar() {
+
+    }
+}
