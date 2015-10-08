84f497fe2c9c8f88d99a6eee3e1b488e30a4b6e8
Harlber <1044698769@qq.com>
2015-10-08 23:59:52 +0800

    Begin  UncaughtExceptionHandler

---
+++ app/src/main/java/moose/com/ac/util/UncaughtHandler.java
@@ -0,0 +1,12 @@
+package moose.com.ac.util;
+
+/**
+ * Created by Farble on 2015/10/8 23.
+ */
+public class UncaughtHandler implements Thread.UncaughtExceptionHandler {
+    private static final String TAG = "UncaughtHandler";
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+
+    }
+}
