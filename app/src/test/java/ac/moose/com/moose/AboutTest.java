4b6284e7cc9138596c1b5889e3f2c74fc903ce2d
Harlber <1044698769@qq.com>
2015-12-09 15:23:35 +0800

    Add AboutTest

---
+++ app/src/test/java/ac/moose/com/moose/AboutTest.java
@@ -0,0 +1,30 @@
+package ac.moose.com.moose;
+
+import android.widget.TextView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import moose.com.ac.About;
+import moose.com.ac.R;
+
+import static org.assertj.android.api.Assertions.assertThat;
+
+/**
+ * Created by dell on 2015/12/9.
+ * Unit Test for {@link About}
+ */
+@RunWith(RobolectricTestRunner.class)
+public class AboutTest {
+    @Test
+    public void test() {
+        About activity = Robolectric.setupActivity(About.class);
+        assertThat((TextView) activity.findViewById(R.id.text_application_info)).containsText("Version");
+        assertThat((TextView) activity.findViewById(R.id.text_developer_info)).containsText("咲くやこの花");
+        assertThat((TextView) activity.findViewById(R.id.text_libraries)).isNotEmpty();
+        assertThat((TextView) activity.findViewById(R.id.text_libraries)).containsText("Square");
+        assertThat((TextView) activity.findViewById(R.id.text_3rd_party_licenses)).containsText("License");
+    }
+}
