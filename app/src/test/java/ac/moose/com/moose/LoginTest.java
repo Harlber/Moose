e95699302ac62d228217cb66804f4c19e1281e6b
Harlber <1044698769@qq.com>
2015-12-09 19:43:01 +0800

    Add Login Test

---
+++ app/src/test/java/ac/moose/com/moose/LoginTest.java
@@ -0,0 +1,32 @@
+package ac.moose.com.moose;
+
+import android.support.v7.widget.AppCompatEditText;
+
+import org.junit.Test;
+import org.robolectric.Robolectric;
+
+import moose.com.ac.Login;
+import moose.com.ac.R;
+import moose.com.ac.common.Config;
+import moose.com.ac.ui.widget.EmailEditText;
+import moose.com.ac.util.PreferenceUtil;
+
+import static org.assertj.android.api.Assertions.assertThat;
+
+/**
+ * Created by dell on 2015/12/9.
+ * Unit Test for {@link Login}
+ */
+public class LoginTest {
+    @Test
+    public void clickingLogin_shouldGetLoginStatus() {
+        Login activity = Robolectric.setupActivity(Login.class);
+        EmailEditText emailEditText = (EmailEditText) activity.findViewById(R.id.login_name);
+        emailEditText.setText("username");
+        AppCompatEditText appCompatEditText = (AppCompatEditText) activity.findViewById(R.id.login_pwd);
+        appCompatEditText.setText("123456");
+        activity.findViewById(R.id.login_submit).performClick();
+
+        assertThat(PreferenceUtil.getPreferences()).contains(Config.LOGIN_STATUS, Config.LOGIN_IN);
+    }
+}
