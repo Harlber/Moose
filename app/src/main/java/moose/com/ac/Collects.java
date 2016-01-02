47710d7c84e31e9fb5bb4664e28ecd0bc51d9f28
Harlber <1044698769@qq.com>
2016-01-02 23:18:57 +0800

    Add License in file

--- app/src/main/java/moose/com/ac/Collects.java
+++ app/src/main/java/moose/com/ac/Collects.java
@@ -20,7 +20,21 @@ import moose.com.ac.util.SortUtil;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
-
+/*
+ * Copyright Farble Dast. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * Created by dell on 2015/8/25.
  * local collection
