514ea1ee5e2b1d7b64d6397f8b11fab2737e05ca
huangyuexin <448468458@qq.com>
2017-12-18 11:28:38 +0800

    修复云记录列表两个按钮的bug

--- app/src/main/java/moose/com/ac/ui/CommentListFragment.java
+++ app/src/main/java/moose/com/ac/ui/CommentListFragment.java
@@ -106,7 +106,7 @@ public class CommentListFragment extends BaseListFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.abs_list_fragment, container, false);
+        return inflater.inflate(R.layout.abs_list_floating_fragment, container, false);
     }
 
     /**
